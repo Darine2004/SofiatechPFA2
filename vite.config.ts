a6ade085f451413c80b94b81c9a7da70fa792132
Darine2004 <darinerezgui3@gmail.com>
2026-04-28 21:35:13 +0100

    Fix vite entry point for Firebase build

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,20 @@
-import { defineConfig } from 'vite'
+﻿import { defineConfig } from 'vite'
 import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [
-    // The React and Tailwind plugins are both required for Make, even if
-    // Tailwind is not being actively used – do not remove them
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      // Alias @ to the src directory
       '@': path.resolve(__dirname, './src'),
     },
   },
-  base: '/SofiatechPFA2/',   // ← AJOUTE CETTE LIGNE ICI
-
-  // File types to support raw imports. Never add .css, .tsx, or .ts files to this.
+  base: '/SofiatechPFA2/',
+  build: {
+    rollupOptions: {
+      input: path.resolve(__dirname, 'src/app/main.tsx'),
+    },
+  },
   assetsInclude: ['**/*.svg', '**/*.csv'],
 })
